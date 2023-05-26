9086b39cf4eb26f23ab78f099107789f204beb83
Ethan Black <ethanrossblack@gmail.com>
2023-05-26 15:31:50 -0600

    feat: add Renderer module and methods

---
+++ lib/renderer.rb
@@ -0,0 +1,32 @@
+module Renderer
+
+  def welcome_message
+    puts "\nWelcome to CONNECT 4\n\n"
+  end
+
+  def render_board(board)
+    puts "A B C D E F G"
+
+    counter = 5
+
+    while counter >= 0
+      row = ""
+      board.columns.keys.each do |key|
+        row += render_cell(board.columns[key][counter]) + " "
+      end
+      # require "pry"; binding.pry
+      puts row
+      counter -= 1
+    end
+  end
+
+  def render_cell(cell)
+    if cell.empty?
+      "."
+    elsif cell.ply_1?
+      "X"
+    elsif cell.ply_2?
+      "O"
+    end
+  end
+end
