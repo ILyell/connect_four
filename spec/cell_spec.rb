9964484f9303685f948261334c0bd34e054deaac
Ian Lyell <ian.lyell@yahoo.com>
2023-05-25 21:20:29 -0500

    test: Reader methods for Cell Class

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -9,14 +9,40 @@ RSpec.describe Cell do
     
     describe '#initialize' do
         it 'exist' do
+            
             expect(@cell_1).to be_a(Cell)
             expect(@cell_2).to be_a(Cell)
+
         end
 
         it 'starts with 1 attribute(@status) with a value of :empty' do
 
             expect(@cell_1.status).to eq(:empty)
             expect(@cell_2.status).to eq(:empty)
+
+        end
+    end
+
+    describe 'reader methods for getting a boolean from @status' do
+        it '#empty?' do
+
+            expect(@cell_1.empty?).to eq(true)
+            expect(@cell_2.empty?).to eq(true)
+
+        end
+
+        it '#ply_1?' do
+            
+            expect(@cell_1.ply_1?).to eq(false)
+            expect(@cell_2.ply_1?).to eq(false)
+
+        end
+
+        it '#ply_2?' do 
+            
+            expect(@cell_2.play_2?).to eq(false)
+            expect(@cell_2.play_2?).to eq(false)
+
         end
     end
 end
