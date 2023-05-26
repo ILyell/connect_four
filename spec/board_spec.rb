2cc364a59af33fcc290fd58a8f2ecf61d82afac2
Ian Lyell <ian.lyell@yahoo.com>
2023-05-26 15:32:29 -0500

    test: Board class #populate method

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 RSpec.describe do
     before do 
         @board = Board.new
+
     end
 
     describe '#initialize' do
@@ -20,6 +21,24 @@ RSpec.describe do
             expect(@board.col_f).to eq([])
             expect(@board.col_g).to eq([])
 
+        end
+    end
+
+    describe '#populate' do
+        it 'fills each column with 6 new cell objects' do
+            
+            @board.populate
+
+            expect(@board.col_a).to be_a([Cell, Cell, Cell, Cell, Cell , Cell])
+
+            expect(@board.col_b).to be_a([Cell, Cell, Cell, Cell, Cell , Cell])
+            expect(@board.col_c).to be_a([Cell, Cell, Cell, Cell, Cell , Cell])
+            expect(@board.col_d).to be_a([Cell, Cell, Cell, Cell, Cell , Cell])
+            expect(@board.col_e).to be_a([Cell, Cell, Cell, Cell, Cell , Cell])
+            expect(@board.col_f).to be_a([Cell, Cell, Cell, Cell, Cell , Cell])
+            expect(@board.col_g).to be_a([Cell, Cell, Cell, Cell, Cell , Cell])
+
+            
         end
     end
 end
