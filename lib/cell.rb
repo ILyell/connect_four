ae956d87f403c6fa7e136431cfa30bb2c91aedc3
Ian Lyell <127703036+ILyell@users.noreply.github.com>
2023-05-26 12:07:15 -0500

    Update lib/cell.rb
    Co-authored-by: Ethan Black <ethanrossblack@gmail.com>

--- lib/cell.rb
+++ lib/cell.rb
@@ -11,11 +11,7 @@ class Cell
     end
 
     def ply_1?
-        if @status == :ply_1
-            true
-        else
-            false
-        end
+        @status == :ply_1
     end
 
     def ply_2?
