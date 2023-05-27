03ad0f69e93627ca36aedd3550760aeab7e50bf9
Ian Lyell <ian.lyell@yahoo.com>
2023-05-26 20:15:46 -0500

    refactor: Render render_board refactor. Not pretty but works better then old loop

--- lib/renderer.rb
+++ lib/renderer.rb
@@ -4,25 +4,19 @@ module Renderer
     puts "\nWelcome to CONNECT 4\n\n"
   end
 
-  def render_board(board)
-    puts " A B C D E F G"
-
-    counter = 5
-
-    while counter >= 0
-      row = ""
-      board.columns.keys.each do |key|
-        row += " #{render_cell(board.columns[key][counter])}"
-      end
-      # require "pry"; binding.pry
-      puts "\r #{row}"
-      counter -= 1
-    end
+  def render_board(board) 
+    system "clear"
+    print " A B C D E F G\n"
+    print " #{render_cell(board.columns[:a][5])} #{render_cell(board.columns[:b][5])} #{render_cell(board.columns[:c][5])} #{render_cell(board.columns[:d][5])} #{render_cell(board.columns[:e][5])} #{render_cell(board.columns[:f][5])} #{render_cell(board.columns[:g][5])}\n"
+    print " #{render_cell(board.columns[:a][4])} #{render_cell(board.columns[:b][4])} #{render_cell(board.columns[:c][4])} #{render_cell(board.columns[:d][4])} #{render_cell(board.columns[:e][4])} #{render_cell(board.columns[:f][4])} #{render_cell(board.columns[:g][4])}\n"
+    print " #{render_cell(board.columns[:a][3])} #{render_cell(board.columns[:b][3])} #{render_cell(board.columns[:c][3])} #{render_cell(board.columns[:d][3])} #{render_cell(board.columns[:e][3])} #{render_cell(board.columns[:f][3])} #{render_cell(board.columns[:g][3])}\n"
+    print " #{render_cell(board.columns[:a][2])} #{render_cell(board.columns[:b][2])} #{render_cell(board.columns[:c][2])} #{render_cell(board.columns[:d][2])} #{render_cell(board.columns[:e][2])} #{render_cell(board.columns[:f][2])} #{render_cell(board.columns[:g][2])}\n"
+    print " #{render_cell(board.columns[:a][1])} #{render_cell(board.columns[:b][1])} #{render_cell(board.columns[:c][1])} #{render_cell(board.columns[:d][1])} #{render_cell(board.columns[:e][1])} #{render_cell(board.columns[:f][1])} #{render_cell(board.columns[:g][1])}\n"
+    print " #{render_cell(board.columns[:a][0])} #{render_cell(board.columns[:b][0])} #{render_cell(board.columns[:c][0])} #{render_cell(board.columns[:d][0])} #{render_cell(board.columns[:e][0])} #{render_cell(board.columns[:f][0])} #{render_cell(board.columns[:g][0])}"
   end
-
   def render_cell(cell)
     if cell.empty?
-      "."
+      "*"
     elsif cell.ply_1?
       "X"
     elsif cell.ply_2?
