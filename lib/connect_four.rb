b397d8da113cf104504af98ffa8997548bb91ab7
Ethan Black <ethanrossblack@gmail.com>
2023-05-27 09:43:25 -0500

    feat: add #prompt_user from Turn to interaction pattern

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,9 +1,11 @@
-
-require_relative "board"
-require_relative "cell"
+require_relative "board.rb"
+require_relative "cell.rb"
+require_relative "renderer.rb"
+require_relative "turn.rb"
 require "pry"
-require_relative 'renderer'
+
 include Render
+
 board = Board.new
 
 board.welcome_message
@@ -12,28 +14,72 @@ board.populate_columns
 
 render_board(board)
 
-board.columns[:a][0].set_ply(:ply_1)
+turn = Turn.new(:ply_1, board)
+turn.prompt_user
+turn = Turn.new(:ply_2, board)
+turn.prompt_user
+turn = Turn.new(:ply_1, board)
+turn.prompt_user
+turn = Turn.new(:ply_2, board)
+turn.prompt_user
+turn = Turn.new(:ply_1, board)
+turn.prompt_user
+turn = Turn.new(:ply_2, board)
+turn.prompt_user
+turn = Turn.new(:ply_1, board)
+turn.prompt_user
+turn = Turn.new(:ply_2, board)
+turn.prompt_user
+turn = Turn.new(:ply_1, board)
+turn.prompt_user
+turn = Turn.new(:ply_2, board)
+turn.prompt_user
+turn = Turn.new(:ply_1, board)
+turn.prompt_user
+turn = Turn.new(:ply_2, board)
+turn.prompt_user
+turn = Turn.new(:ply_1, board)
+turn.prompt_user
+turn = Turn.new(:ply_2, board)
+turn.prompt_user
+turn = Turn.new(:ply_1, board)
+turn.prompt_user
+turn = Turn.new(:ply_2, board)
+turn.prompt_user
+turn = Turn.new(:ply_1, board)
+turn.prompt_user
+turn = Turn.new(:ply_2, board)
+turn.prompt_user
+turn = Turn.new(:ply_1, board)
+turn.prompt_user
+turn = Turn.new(:ply_2, board)
+turn.prompt_user
 
-render_board(board)
 
-board.add_piece(:ply_2, :b)
-board.render_board(board)
-board.add_piece(:ply_1, :b)
-render_board(board)
-board.add_piece(:ply_2, :b)
-render_board(board)
-board.add_piece(:ply_1, :b)
-render_board(board)
-board.add_piece(:ply_2, :d)
-render_board(board)
-board.add_piece(:ply_1, :e)
-render_board(board)
-board.add_piece(:ply_2, :g)
-render_board(board)
-board.add_piece(:ply_1, :b)
-render_board(board)
-board.add_piece(:ply_2, :a)
-render_board(board)
+
+
+# board.columns[:a][0].set_ply(:ply_1)
+
+# render_board(board)
+
+# board.add_piece(:ply_2, :b)
+# board.render_board(board)
+# board.add_piece(:ply_1, :b)
+# render_board(board)
+# board.add_piece(:ply_2, :b)
+# render_board(board)
+# board.add_piece(:ply_1, :b)
+# render_board(board)
+# board.add_piece(:ply_2, :d)
+# render_board(board)
+# board.add_piece(:ply_1, :e)
+# render_board(board)
+# board.add_piece(:ply_2, :g)
+# render_board(board)
+# board.add_piece(:ply_1, :b)
+# render_board(board)
+# board.add_piece(:ply_2, :a)
+# render_board(board)
 
 
 
